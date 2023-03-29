b34413b6331028fbbc0cb4fd25e7190d248f0134
Pedro Henrique <78853588+pedrohemmel@users.noreply.github.com>
2023-03-29 15:21:13 -0300

    fix: Organizando código

--- Mini_Challenge-3/Mini_Challenge-3/View/TelaDesenhar/TelaDesenharView.swift
+++ Mini_Challenge-3/Mini_Challenge-3/View/TelaDesenhar/TelaDesenharView.swift
@@ -45,14 +45,12 @@ struct TelaDesenharView: View {
                     }
                 }
             }
-            
             VStack {
                 SliderComponente(titulo: "Opacidade", medida: self.$opacidade)
                 SliderComponente(titulo: "Lupa", medida: self.$escalaZoom)
             }
             .onChange(of: escalaZoom) { escala in
-                var novaEscala = 1 + 6 * (1 - escala)
-                alterarZoom(escala: CGFloat(novaEscala))
+                alterarZoom(escala: CGFloat(escala))
             }
             
         }
@@ -63,9 +61,10 @@ struct TelaDesenharView: View {
     }
     
     func alterarZoom(escala: CGFloat) {
+        let novaEscala = 1 + 6 * (1 - escala)
         do {
             try camera?.lockForConfiguration()
-            camera?.videoZoomFactor = escala
+            camera?.videoZoomFactor = novaEscala
             camera?.unlockForConfiguration()
         } catch {
             print("Failed to update zoom: \(error.localizedDescription)")
@@ -79,83 +78,3 @@ struct TelaDesenharView: View {
         return false
     }
 }
-
-//
-//struct ModoCameraView: View {
-//    let larguraTela = UIScreen.main.bounds.size.width
-////   VARIAVEIS QUE ACABEU DE CRIAR PARA A CAMERA, UMA DE ZOOM E A OUTRA PARA ACESSAR A CAMERA
-//    let cameraDevice = AVCaptureDevice.default(.builtInDualWideCamera, for: .video, position: .back)
-//    @State private var zoomScale: CGFloat = 1.0
-//
-//    @StateObject var cameraViewModel = ModoCameraViewModel()
-//
-//    var body: some View {
-//        ZStack{
-//        VStack{
-//            ModoCameraModel(cameraViewModel: cameraViewModel)
-//                .ignoresSafeArea(.all, edges: .all)
-//
-//                Rectangle()
-//                    .foregroundColor(.white)
-//                    .frame(width: larguraTela, height: 300, alignment: .center)
-//                    .opacity(0.5)
-//
-//                Circle()
-//                    .scale(0.5)
-//                    .stroke(.black, lineWidth: 3)
-//                    .opacity(0.5)
-////            Rectangle()
-////                .foregroundColor(.white)
-////                .frame(width: larguraTela, height: 300, alignment: .center)
-////                .opacity(0.5)
-////
-////            Circle()
-////                .scale(0.5)
-////                .stroke(.black, lineWidth: 3)
-////                .opacity(0.5)
-//
-//            Image(systemName: "plus.magnifyingglass")
-//                .font(.title)
-//                .foregroundColor(.white)
-//                .padding()
-//                .background(Color.black.opacity(0.5))
-//                .cornerRadius(10)
-////            TAP GESTURE DO ZOOM QUE ACABEI DE CRIAR
-//                .onTapGesture {
-//                    zoomScale += 0.1
-//                                                            updateZoom()
-//                }
-//            Image(systemName: "minus.magnifyingglass")
-//                .font(.title)
-//                .foregroundColor(.white)
-//                .padding()
-//                .background(Color.black.opacity(0.5))
-//                .cornerRadius(10)
-//            //            TAP GESTURE DO ZOOM QUE ACABEI DE CRIAR
-//                .onTapGesture {
-//                    zoomScale -= 0.1
-//                                                            updateZoom()
-//                }
-//                .onAppear(perform: cameraViewModel.checarPermissao)
-//            //        .alert(isPresented: $cameraViewModel.alerta){
-//            //            Alert(title: Text("Please enable cameraModel access"))
-//            //        }
-//        }
-//
-//    }
-//
-////    FUNCAO DO ZOOM QUE ACABEI DE CRIAR
-//    func updateZoom() {
-//        do {
-//            try cameraDevice?.lockForConfiguration()
-//            cameraDevice?.videoZoomFactor = zoomScale
-//            cameraDevice?.unlockForConfiguration()
-//        } catch {
-//            print("Failed to update zoom: \(error.localizedDescription)")
-//        }
-//        .onAppear(perform: cameraViewModel.checarPermissao)
-////        .alert(isPresented: $cameraViewModel.alerta){
-////            Alert(title: Text("Please enable cameraModel access"))
-////        }
-//    }
-//}
