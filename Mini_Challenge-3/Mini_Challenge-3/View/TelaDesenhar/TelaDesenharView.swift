207b38127e89b526da4fb6d76880d2a8bf2ac968
Pedro Henrique <78853588+pedrohemmel@users.noreply.github.com>
2023-03-29 15:33:31 -0300

    developing: Desenvolvendo design da tela de desenhar

--- Mini_Challenge-3/Mini_Challenge-3/View/TelaDesenhar/TelaDesenharView.swift
+++ Mini_Challenge-3/Mini_Challenge-3/View/TelaDesenhar/TelaDesenharView.swift
@@ -14,11 +14,12 @@ struct TelaDesenharView: View {
     @State var eDesenho: Bool? = nil
     
     @State private var escalaZoom = 1.0
-    @State private var opacidade = 0.5
+    @State private var opacidadeFundo = 0.5
+    @State private var opacidadeImagem = 0.5
     
     @StateObject var cameraViewModel = ModoCameraViewModel()
+    let telaDesenharViewModel = TelaDesenharViewModel()
     
-    let camera = AVCaptureDevice.default(.builtInDualWideCamera, for: .video, position: .back)
     let larguraTela = UIScreen.main.bounds.size.width
     let alturaTela = UIScreen.main.bounds.size.height
     
@@ -29,52 +30,44 @@ struct TelaDesenharView: View {
             Rectangle()
                 .frame(width: larguraTela, height: alturaTela)
                 .background(.white)
-                .opacity(opacidade)
+                .opacity(self.opacidadeFundo)
             VStack {
                 if self.eDesenho ?? false {
                     Image(self.desenhoSelecionado)
                         .resizable()
                         .frame(width: larguraTela, height: larguraTela)
-                        .opacity(self.opacidade)
+                        .opacity(self.opacidadeImagem)
                 } else {
                     if let uiImage = UIImage(data: self.dadosImagemSelecionada) {
                         Image(uiImage: uiImage)
                             .resizable()
                             .frame(width: larguraTela, height: larguraTela)
-                            .opacity(self.opacidade)
+                            .opacity(self.opacidadeImagem)
                     }
                 }
             }
             VStack {
-                SliderComponente(titulo: "Opacidade", medida: self.$opacidade)
+                
                 SliderComponente(titulo: "Lupa", medida: self.$escalaZoom)
+                SliderComponente(titulo: "Fundo", medida: self.$opacidadeFundo)
+                SliderComponente(titulo: "Imagem", medida: self.$opacidadeImagem)
             }
+            .padding()
+            .background(.gray)
+            .cornerRadius(10)
+            .padding()
             .onChange(of: escalaZoom) { escala in
-                alterarZoom(escala: CGFloat(escala))
+                self.telaDesenharViewModel.alterarZoom(escala: CGFloat(escala))
             }
             
         }
         .onAppear {
-            self.eDesenho = self.verificaImagemNula(dadosImagemSelecionada: self.dadosImagemSelecionada, desenhoSelecionado: self.desenhoSelecionado)
+            self.eDesenho = self.telaDesenharViewModel.verificaImagemNula(dadosImagemSelecionada: self.dadosImagemSelecionada, desenhoSelecionado: self.desenhoSelecionado)
         }
         .onAppear(perform: cameraViewModel.checarPermissao)
     }
     
-    func alterarZoom(escala: CGFloat) {
-        let novaEscala = 1 + 6 * (1 - escala)
-        do {
-            try camera?.lockForConfiguration()
-            camera?.videoZoomFactor = novaEscala
-            camera?.unlockForConfiguration()
-        } catch {
-            print("Failed to update zoom: \(error.localizedDescription)")
-        }
-    }
     
-    func verificaImagemNula(dadosImagemSelecionada: Data?, desenhoSelecionado: String?) -> Bool {
-        if desenhoSelecionado != "" {
-            return true
-        }
-        return false
-    }
+    
+    
 }
